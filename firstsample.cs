9de7b2ab6aec7718e2f555dd0716859cfb933326
dinthavong2 <dennis.inthavong@gmail.com>
2021-01-13 11:11:21 -0700

    Sample of using a variable in a WriteLine command

---
+++ firstsample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This course section is:\tOA03");
+
+            string myName = "Dennis Inthavong";
+            Console.WriteLine($"This name is printed from a variable is\t{myName}");
+               
+            
+        }
+    }
+}
