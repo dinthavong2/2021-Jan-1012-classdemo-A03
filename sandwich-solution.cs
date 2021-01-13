77f343cc893eacbead40f6e5cb5bd0ac5364e4fc
dinthavong2 <dennis.inthavong@gmail.com>
2021-01-13 10:53:12 -0700

    Create sandwich-solution.cs

---
+++ sandwich-solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How to make a sandwich of your choice");
+            Console.WriteLine("Step 1: \t Decide if the sandwich needs to be cooked or not");
+            Console.WriteLine("Step 2: \t Choose the type of bread");
+            Console.WriteLine("Step 3: \t Choose the type of meat");
+            Console.WriteLine("Step 4: \t Choose the type of cheese");
+            Console.WriteLine("Step 5: \t Choose the condiments");
+            Console.WriteLine("Step 6: \t Grab a cutting knife");
+            Console.WriteLine("Step 7: \t Cut the bread in half");
+            Console.WriteLine("Step 8: \t Add the meat on one of the cut bread slices");
+            Console.WriteLine("Step 9: \t Add slice of cheese on top of the placed meat");
+            Console.WriteLine("Step 10: \t Add ketchup and mustard on top of placed cheese");
+            Console.WriteLine("Step 11: \t Close the sandwich by putting other half of cut bread on top of cheese");
+            Console.WriteLine("Step 12: \t Put the knife in the sink and put away all sandwich ingredients back.");
+         
+
+
+        }
+    }
+}
